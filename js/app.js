07c1c554151213f220f1f870eda01060130df7c1
OMAR <omar.ahmed.khalaf2006@gmail.com>
2022-10-27 18:40:18 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel();
+  });
+
+
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+    speedAsDuration: true
+  });
+
+
+  new WOW().init();
